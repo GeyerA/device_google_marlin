dd78aff040dccef47ba0a77a84f1bc2b2373c468
Andreas Geyer <calo.geyer@gmail.com>
2017-10-22 21:57:57 +0200

    Added overlays, aosp_fixes and build gapps
    Change-Id: Ib918db628699e19da72ca099905172edb4a0705e

--- aosp_sailfish.mk
+++ aosp_sailfish.mk
@@ -34,7 +34,12 @@ PRODUCT_RESTRICT_VENDOR_FILES := true
 PRODUCT_COPY_FILES += device/google/marlin/fstab.common:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.sailfish
 
 $(call inherit-product, device/google/marlin/device-sailfish.mk)
-$(call inherit-product-if-exists, vendor/google_devices/marlin/device-vendor-sailfish.mk)
+$(call inherit-product-if-exists, vendor/google_devices/sailfish/device-vendor-sailfish.mk)
+$(call inherit-product-if-exists, vendor/google_devices/sailfish/aosp_fixes.mk)
+# Inherit prebuilt apps
+$(call inherit-product-if-exists, vendor/gapps/prebuilt.mk)
+# Inherit arm64 phone gapps
+$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
 
 PRODUCT_PACKAGES += \
     Launcher3 \
